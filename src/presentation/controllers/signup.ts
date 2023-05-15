8ee7b2be1fb580d3e0062dbb52459b6728e6679b
dougszz <akirozzzz1998@gmail.com>
2023-05-14 22:17:22 -0300

    feat: add new features

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,27 @@
+import { type HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-errors'
+import { badRequest } from '../helpers/http-helper'
+import { type Controller } from '../protocols/controller'
+import { type EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
-export class SignUpController {
-  handle (httpRequest: any): any {
-    if (!httpRequest.body.name) { // eslint-disable-line
-      return {
-        statusCode: 400,
-        body: new Error('Missing params: name')
+export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  handle (httpRequest: HttpRequest): any {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) { // eslint-disable-line
+        return badRequest(new MissingParamError(field))
       }
     }
-    if (!httpRequest.body.email) { // eslint-disable-line
-      return {
-        statusCode: 400,
-        body: new Error('Missing params: email')
-      }
+    const isValid = this.emailValidator.isvalid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
